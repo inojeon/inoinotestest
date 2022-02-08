8d72234ceffdd6d80eac34c221a0d2bd2f8d9d52
inojeon <inojeon@kisti.re.kr>
2022-02-08 21:37:29 +0900

    change port 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const next = require("next");
 
 const net = require("net");
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = parseInt(process.env.PORT, 10) || 8080;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
